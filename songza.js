c90922faf82e55e1c62a6a5c6a33d5ad178c99ed
Trevor Senior <trevor@tsenior.com>
2014-05-16 13:00:40 -0400

    added in new endpoints

--- songza.js
+++ songza.js
@@ -3,11 +3,13 @@
 var request = require('./request');
 
 exports.artist = require('./api/artist');
+exports.auth = require('./api/auth');
 exports.collection = require('./api/collection');
 exports.gallery = require('./api/gallery');
+exports.promotion = require('./api/promotion');
 exports.search = require('./api/search');
 exports.situation = require('./api/situation');
 exports.song = require('./api/song');
 exports.station = require('./api/station');
-exports.tags = require('./api/tags');
+exports.tag = require('./api/tag');
 exports.user = require('./api/user');
